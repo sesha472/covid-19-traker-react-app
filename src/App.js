902e75be09523d4faca5cc026ca98bdfb1365b20
sesha <sesha472@gmail.com>
2021-08-22 18:19:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react';
 
 function App() {
+  const [data, setdata] = useState([]);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    // fetch('https://api.covid19india.org/data.json') this api is not working now 
+
+    .then( res => res.json()
+    ).then(jsondata => setdata(jsondata))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SESHA COVID-19 TRAKER REACT APP</h1>
+      
+      <table className="table table-hover" style={{width:"50%",margin:'auto'}}>
+  <thead>
+    <tr>
+      <th scope="col">userId</th>
+      <th scope="col">title</th>
+      <th scope="col">completed</th>
+      <th scope="col">id</th>
+    </tr>
+  </thead>
+  <tbody>
+    {data.map(user=>{
+      return(
+        <tr>
+        <th scope="row">{user.userId}</th>
+        <td>{user.title}</td>
+        <td>{(user.completed)?<p>completed</p>:<p>notcompleted</p>}</td>
+        <td>{user.id}</td>
+      </tr>
+      )
+    })}
+    
+  </tbody>
+</table>
+      
     </div>
   );
 }
